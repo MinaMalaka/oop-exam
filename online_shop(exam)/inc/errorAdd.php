70addb11798902690147994ede18d13ac616963b
MinaMalaka <153759949+MinaMalaka@users.noreply.github.com>
2024-08-30 08:33:29 +0300

    Add files via upload

---
+++ online_shop(exam)/inc/errorAdd.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="../add.php " method='post' class="form">
+        <input type="submit" value="return to add page" name="submit" class="btn btn-primary w-100 mt-5 text-center">
+    </form>
+
+</body>
+
+</html>
---
+++ online_shop(exam)/inc/errorEdit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="../edit.php?id=<?php echo $id ?> " method='post' class="form">
+        <input type="submit" value="return to edit page" name="submit" class="btn btn-primary w-100 mt-5 text-center">
+    </form>
+
+</body>
+
+</html>
